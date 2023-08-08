d41177b72aca72ea2155257c54ea74ea789e98da
mbpswangxg <xg.wang@mbpsmartec.co.jp>
2023-08-08 15:48:39 +0900

    add MatchText(pattern, content), ReplaceText(pattern, content, replacement)

--- cmd/match.go
+++ cmd/match.go
@@ -5,29 +5,98 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bytes"
 	"fmt"
-
+	"regexp"
 	"github.com/spf13/cobra"
 )
 
 // matchCmd represents the match command
 var matchCmd = &cobra.Command{
 	Use:   "match",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "match string by regrep",
+	Long: ` match string by regrep pattern, and replace string if parameter --replace is set.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("match called")
+		fmt.Println("regex match called")
+		
+		pattern, _ := cmd.Flags().GetString("pattern")
+		content, _ := cmd.Flags().GetString("content")
+		replace, _ := cmd.Flags().GetString("replace")
+		
+		fmt.Printf("args=%v, args.length=%d pattern=%s,  content=%s, replace=%s \n", args, len(args), pattern, content, replace)
+	
+		MatchText(pattern, content)
+		ReplaceText(pattern, content, replace)
 	},
+}
+func MatchText(pattern string, content string) {
+
+    match, _ := regexp.MatchString(pattern, content)
+    fmt.Println(match)
+
+    r, _ := regexp.Compile(pattern)
+
+    fmt.Println(r.MatchString(content))
+
+    fmt.Println(r.FindString(content))
+
+    fmt.Println(r.FindStringIndex(content))
+
+    fmt.Println(r.FindStringSubmatch(content))
+
+    fmt.Println(r.FindStringSubmatchIndex(content))
+
+    fmt.Println(r.FindAllString(content, -1))
+
+    fmt.Println(r.FindAllStringSubmatchIndex(content, -1))
+
+    fmt.Println(r.FindAllString(content, 2))
+
+    fmt.Println(r.Match([]byte(content)))
+
+}
+
+func ReplaceText(pattern string, content string, replacement string) {
+
+    match, _ := regexp.MatchString(pattern, content)
+    fmt.Println(match)
+
+    // r, _ := regexp.Compile(pattern)
+
+    // fmt.Println(r.MatchString(content))
+
+    // fmt.Println(r.FindString(content))
+
+    // fmt.Println(r.FindStringIndex(content))
+
+    // fmt.Println(r.FindStringSubmatch(content))
+
+    // fmt.Println(r.FindStringSubmatchIndex(content))
+
+    // fmt.Println(r.FindAllString(content, -1))
+
+    // fmt.Println(r.FindAllStringSubmatchIndex(content, -1))
+
+    // fmt.Println(r.FindAllString(content, 2))
+
+    // fmt.Println(r.Match([]byte(content)))
+
+    r := regexp.MustCompile(pattern)
+    fmt.Println(r)
+
+    fmt.Println(r.ReplaceAllString(content, replacement))
+
+    in := []byte(content)
+    out := r.ReplaceAllFunc(in, bytes.ToUpper)
+    fmt.Println(string(out))
 }
 
 func init() {
 	regexCmd.AddCommand(matchCmd)
-
+	matchCmd.Flags().StringP("pattern", "p","", "regex pattern string")
+	matchCmd.Flags().StringP("content", "c","", "input content string")
+	matchCmd.Flags().StringP("replace", "r","", "regex replace string")
+	
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
